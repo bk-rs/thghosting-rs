2bfeec168dbf520a7d5327fb2e7b872c241b6c3c
vkill <vkill.net@gmail.com>
2021-06-24 11:26:30 +0800

    Update parse

--- thghosting-data-centers/src/lib.rs
+++ thghosting-data-centers/src/lib.rs
@@ -1,11 +1,15 @@
-use std::{io::Error as IoError, time::Duration};
+use std::{
+    io::Error as IoError,
+    net::{AddrParseError, Ipv4Addr},
+    time::Duration,
+};
 
 use isahc::{
     config::Configurable as _, AsyncReadResponseExt as _, Error as IsahcError, HttpClient,
 };
 use scraper::{Html, Selector};
 
-const FETCH_URL: &str = "https://www.thghosting.com/network/data-centers/";
+pub const FETCH_URL: &str = "https://www.thghosting.com/network/data-centers/";
 
 pub async fn fetch() -> Result<String, FetchError> {
     let client = HttpClient::builder()
@@ -27,35 +31,128 @@ pub enum FetchError {
     IoError(#[from] IoError),
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub enum AvailableService {
+    BareMetalServers,
+    VirtualServers,
+    PrivateCloud,
+}
+
 #[derive(Debug, Clone)]
 pub struct DataCenter {
     pub id: String,
     pub city: String,
+    pub available_services: Vec<AvailableService>,
+    pub standard_bare_metal_bandwidth: Option<String>,
+    pub ping: Option<Ipv4Addr>,
+    pub url: Option<String>,
 }
 
 pub fn parse(html: &str) -> Result<Vec<DataCenter>, ParseError> {
     let document = Html::parse_document(html);
 
-    let selector = Selector::parse("div.location").unwrap();
+    let location_selector = Selector::parse("div.location").unwrap();
 
     let mut data_centers = vec![];
 
-    for element in document.select(&selector) {
-        let id = element
+    'locations: for location_element in document.select(&location_selector) {
+        let id = location_element
             .value()
             .attr("id")
             .ok_or(ParseError::IdMissing)?
             .to_owned();
 
         let city_selector = Selector::parse(".dc-city").unwrap();
-
-        let city = element
+        let city = location_element
             .select(&city_selector)
             .next()
             .ok_or(ParseError::CityMissing)?
             .inner_html();
 
-        data_centers.push(DataCenter { id, city });
+        let mut available_services: Vec<AvailableService> = vec![];
+        let mut standard_bare_metal_bandwidth: Option<String> = None;
+        let mut ping: Option<Ipv4Addr> = None;
+
+        let tr_selector = Selector::parse("table tr").unwrap();
+        for tr_element in location_element.select(&tr_selector) {
+            let td_selector = Selector::parse("td").unwrap();
+            let mut td_element_iter = tr_element.select(&td_selector);
+            let head_element = td_element_iter
+                .next()
+                .ok_or(ParseError::AttrElementInvalid)?;
+            let _ = td_element_iter
+                .next()
+                .ok_or(ParseError::AttrElementInvalid)?;
+            let value_element = td_element_iter
+                .next()
+                .ok_or(ParseError::AttrElementInvalid)?;
+            if td_element_iter.next().is_some() {
+                return Err(ParseError::AttrElementInvalid);
+            }
+            match head_element.inner_html().as_str() {
+                "Available Services" => {
+                    let a_selector = Selector::parse("a").unwrap();
+                    for ele in value_element.select(&a_selector) {
+                        if let Some(title) = ele.value().attr("title") {
+                            match title {
+                                "Bare Metal Servers" => {
+                                    available_services.push(AvailableService::BareMetalServers)
+                                }
+                                "Virtual Servers" => {
+                                    available_services.push(AvailableService::VirtualServers)
+                                }
+                                "Private Cloud" => {
+                                    available_services.push(AvailableService::PrivateCloud)
+                                }
+                                _ => return Err(ParseError::AvailableServiceUnknown),
+                            }
+                        }
+                    }
+                }
+                "Available Networks" => {}
+                "Standard Bare Metal Bandwidth" => {
+                    let s = value_element.inner_html();
+                    match s.as_str() {
+                        "" => {}
+                        _ => {
+                            standard_bare_metal_bandwidth = Some(s);
+                        }
+                    }
+                }
+                "Ping/Trace Route" => {
+                    let s = value_element.inner_html();
+                    match s.as_str() {
+                        "-" | "" => {
+                            continue 'locations;
+                        }
+                        _ => {
+                            let ipv4_addr =
+                                s.parse().map_err(|err| ParseError::PingInvalid(s, err))?;
+                            ping = Some(ipv4_addr);
+                        }
+                    }
+                }
+                "Certifications" => {}
+                "Test Download" => {}
+                _ => {}
+            }
+        }
+
+        let url_selector = Selector::parse(".popover-container a").unwrap();
+        let url = location_element
+            .select(&url_selector)
+            .next()
+            .and_then(|ele| ele.value().attr("href"))
+            .map(ToOwned::to_owned);
+
+        data_centers.push(DataCenter {
+            id,
+            city,
+            available_services,
+            standard_bare_metal_bandwidth,
+            ping,
+            url,
+        });
     }
 
     Ok(data_centers)
@@ -67,6 +164,12 @@ pub enum ParseError {
     IdMissing,
     #[error("CityMissing")]
     CityMissing,
+    #[error("AttrElementInvalid")]
+    AttrElementInvalid,
+    #[error("AvailableServiceUnknown")]
+    AvailableServiceUnknown,
+    #[error("PingInvalid {0} {1}")]
+    PingInvalid(String, AddrParseError),
 }
 
 #[cfg(test)]
@@ -82,6 +185,20 @@ mod tests {
         let data_centers = parse(html)?;
 
         println!("{:?}", data_centers);
+
+        let dc_london = data_centers
+            .iter()
+            .filter(|dc| dc.id == "london")
+            .next()
+            .unwrap();
+        assert_eq!(
+            dc_london.available_services,
+            vec![
+                AvailableService::BareMetalServers,
+                AvailableService::VirtualServers
+            ]
+        );
+        assert_eq!(dc_london.ping, Some("82.163.78.28".parse().unwrap()));
 
         Ok(())
     }
